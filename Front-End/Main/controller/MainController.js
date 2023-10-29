097dfb7bd8381c2b36b7d79a76527a6320a2eeac
sudeera-madushan <sudeeramadushan888@gmail.com>
2023-10-29 21:53:53 +0530

    main travel problem'

--- Front-End/Main/controller/MainController.js
+++ Front-End/Main/controller/MainController.js
@@ -3,3 +3,38 @@
  * date : 10/29/2023
  * project : Front-End
  */
+
+$(document).ready(function() {
+    getAllAreas();
+});
+
+let getAllAreas=()=>{
+    $.ajax({
+        url: 'http://localhost:8095/travel/api/v1/area',
+        type: 'GET',
+        // headers: {
+        //     "Authorization": `Bearer ${token}`
+        // },
+        success: function (data) {
+            loadAreaCards(data);
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    })
+}
+
+let loadAreaCards=(data)=>{
+    console.log()
+
+    data.object.map((value, index) => {
+        let data=``;
+        console.log(index,index%3===0);
+        if (index%3===0) {
+            data = `<div class="card-columns row">`;
+        }
+        in
+                
+              // </div>`
+    })
+}
