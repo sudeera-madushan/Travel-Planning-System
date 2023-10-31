48ff51dc2986b4245cc2d52dafaa7abc9fa3fe3a
sudeera-madushan <sudeeramadushan888@gmail.com>
2023-10-31 17:14:52 +0530

    get all hotels success

--- Front-End/Main/controller/MainController.js
+++ Front-End/Main/controller/MainController.js
@@ -5,9 +5,10 @@
  */
 
 $(document).ready(function() {
-    // $('#area-list-container').hide();
+    $('#area-list-container').hide();
     $('#area-detail-page').hide();
-    getAllAreas();
+    // getAllAreas();
+    getAllHotel();
 
 });
 $('#areaDetailImageSlidePrev').click(function () {
@@ -182,3 +183,50 @@ let loadAreaDetailsImgList=(arr) => {
 $('#areaDetailsNearPlace').on('click','.card',function () {
     showMoreArea($(this).attr('id'));
 })
+
+let getAllHotel=()=>{
+    $.ajax({
+        url: 'http://localhost:8094/travel/api/v1/hotel',
+        type: 'GET',
+        // headers: {
+        //     "Authorization": `Bearer ${token}`
+        // },
+        success: function (data) {
+            loadHotelCards(data);
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    })
+}
+
+let loadHotelCards=(data)=>{
+
+    data.object.map((value, index) => {
+        console.log(value)
+        $('#hotelCardContainer').empty()
+        let data=``;
+        data=` <div class="card col m-4 p-0" id="${value.id}">
+                    <a href="#">
+                        <img class="card-img-top" src="data:image/jpg;base64, ${value.hotelImages[0].image}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">${value.name}</h5>
+<!--                            <p class="card-text">-->
+<!--                          -->
+<!--                            </p>-->
+                            <p class="card-text">
+                                <small class="text-muted">
+                                    <i class="fas fa-eye"></i>
+                                    1000
+                                    <i class="bx bx-user"></i>
+                                    admin
+                                    <i class="fas fa-calendar-alt"></i>
+                                    Read More
+                                </small>
+                            </p>
+                        </div>
+                    </a>
+                </div>`;
+        $('#hotelCardContainer').append(data)
+    })
+}
--- HotelService/src/main/java/lk/ijse/travel/hotelservice/security/SecurityConfiguration.java
+++ HotelService/src/main/java/lk/ijse/travel/hotelservice/security/SecurityConfiguration.java
@@ -34,9 +34,9 @@ public class SecurityConfiguration {
                 .cors(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(this::corsOriginConfigure))
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("").permitAll()
-                        .requestMatchers("/api/v1/hotel/**").hasRole("HOTEL")
-                        .requestMatchers("/api/v1/option/**").hasRole("HOTEL")
+                        .requestMatchers("**").permitAll()
+//                        .requestMatchers("/api/v1/hotel/**").hasRole("HOTEL")
+//                        .requestMatchers("/api/v1/option/**").hasRole("HOTEL")
                         .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
